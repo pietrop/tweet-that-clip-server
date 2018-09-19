da1857c78da36bde80685f441e9e435a34af2df6
James Dooley <james.dooley@bbc.co.uk>
2018-09-19 15:25:36 +0100

    Adding POST /tweet route, using TTC

---
+++ routes/tweet.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const download = require('download');
+const tweetThatClip = require('tweet-that-clip');
+
+module.exports = (app) => {
+  app.post('/tweet', (req, res) => {
+    const startTime = req.query.start;
+    const endTime = req.query.end;
+    const url = req.query.video;
+
+    download(url).then(data => {
+      const inputFilePath = `dist/${url}`;
+      fs.writeFileSync(inputFilePath, data);
+
+      const opts = {
+        inputFile: inputFilePath,
+        outputFile: `${inputFilePath}_clipped`,
+        inputSeconds: startTime,
+        durationSeconds: (endTime - startTime),
+        tweetText: 'bing bing'
+      };
+
+      tweetThatClip(opts, (err, response) => {
+        if (err) {
+          console.log(err.message);
+          res.sendStatus(500);
+        }
+
+        console.log(response);
+        res.sendStatus(204);
+      });
+    });
+  });
+};
