68a3b33b5d5144bf84a8b0d2d3f7f6eb2f50fc00
James Dooley <james.dooley@bbc.co.uk>
2018-09-19 15:24:31 +0100

    Adding express server

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Content-Type', 'application/json');
+  next();
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+      status: statusCode,
+      message: err.message
+    });
+});
+
+require('./routes/tweet')(app);
+
+module.exports = app;
